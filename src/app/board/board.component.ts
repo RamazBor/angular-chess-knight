a6c8ced1ef927f5c95e36807e2e93840c0a4ed84
Ramaz <ramazborchashvili00@gmail.com>
2022-12-27 12:44:42 +0400

    uploading my project

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent {
+  
+    myImage: String = "assets/Wknight.png";
+
+}
